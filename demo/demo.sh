fae79f8fc2804d65a67e62ae8175ccba7a3c6bea
Tim Shields <tshield@gmail.com>
2011-05-04 22:38:55 -0400

    Updated demo script

--- demo/demo.sh
+++ demo/demo.sh
@@ -3,11 +3,12 @@ sudo mkfs -t ext2 /dev/sdb1
 sudo mount -t tagfs -o user_xattr /dev/sdb1 /media/tagfs
 sudo chown tashields:tashields /media/tagfs
 
-cp dragon.txt /media/tagfs/demo/
-cp panda.txt /media/tagfs/demo/
-cp ice_cream.txt /media/tagfs/demo/
-cp wolf.txt /media/tagfs/demo/
-cp bow.txt /media/tagfs/demo/
+mkdir /media/tagfs/demo
+cp dragon.txt /media/tagfs/demo/dragon.txt
+cp panda.txt /media/tagfs/demo/panda.txt
+cp ice_cream.txt /media/tagfs/demo/ice_cream.txt
+cp wolf.txt /media/tagfs/demo/wolf.txt
+cp bow.txt /media/tagfs/demo/bow.txt
 
 addtag /media/tagfs/demo/dragon.txt animal fire dragon
 addtag /media/tagfs/demo/panda.txt animal bear
@@ -15,10 +16,13 @@ addtag /media/tagfs/demo/ice_cream.txt icecream food cold
 addtag /media/tagfs/demo/wolf.txt animal wolf
 addtag /media/tagfs/demo/bow.txt bow weapon dragon
 
+------------------------------------------------
 distag
 
 chtag dragon
 lstag
+
+tagcat icecream
 
 distag /media/tagfs/demo/bow.txt
 
