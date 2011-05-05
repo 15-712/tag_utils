fac8e7fade5fd980fa0052781faa19a320b28168
Tim Shields <tshield@gmail.com>
2011-05-04 22:31:16 -0400

    Added demo files

---
+++ demo/demo.sh
@@ -0,0 +1,29 @@
+sudo insmod ~/linux-2.6/fs/tagfs/tagfs.ko
+sudo mkfs -t ext2 /dev/sdb1
+sudo mount -t tagfs -o user_xattr /dev/sdb1 /media/tagfs
+sudo chown tashields:tashields /media/tagfs
+
+cp dragon.txt /media/tagfs/demo/
+cp panda.txt /media/tagfs/demo/
+cp ice_cream.txt /media/tagfs/demo/
+cp wolf.txt /media/tagfs/demo/
+cp bow.txt /media/tagfs/demo/
+
+addtag /media/tagfs/demo/dragon.txt animal fire dragon
+addtag /media/tagfs/demo/panda.txt animal bear
+addtag /media/tagfs/demo/ice_cream.txt icecream food cold
+addtag /media/tagfs/demo/wolf.txt animal wolf
+addtag /media/tagfs/demo/bow.txt bow weapon dragon
+
+distag
+
+chtag dragon
+lstag
+
+distag /media/tagfs/demo/bow.txt
+
+rmtag /media/tagfs/demo/bow.txt dragon
+
+lstag
+
+
